0e2c5685ada8cd9dcd7e4e4c280983c0a5016b9b
onnoshakil <shakil2.onnorokom@gmail.com>
2022-06-22 09:44:13 +0600

    initial commit

---
+++ app/src/main/java/xyz/xandsoft/dimvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package xyz.xandsoft.dimvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
